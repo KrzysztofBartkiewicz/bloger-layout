95453daa816e3aa6d9a9c19da0b9e2f129ba6a53
Krzysztof Bartkiewicz <bartkiewicz.krzysztof84@gmail.com>
2020-05-28 20:09:45 +0200

    finished navigation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 const hamburger = document.querySelector('.hamburger');
 const navigation = document.querySelector('.navigation');
 
+const dropdownButton = document.querySelector('.dropdown-button');
+const dropdownMenu = document.querySelector('.dropdown__list');
+
 hamburger.addEventListener('click', (e) => {
    navigation.classList.toggle('visible');
+});
+
+dropdownButton.addEventListener('click', (e) => {
+   dropdownMenu.classList.toggle('visible');
 });
