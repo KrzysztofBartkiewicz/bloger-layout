fd2ddd5057b599fc3150231e300e73b637e6785d
Krzysztof Bartkiewicz <bartkiewicz.krzysztof84@gmail.com>
2020-05-29 18:21:29 +0200

    added section-ab html

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-const hamburger = document.querySelector('.hamburger');
-const navigation = document.querySelector('.navigation');
+const hamburger = document.querySelector(".hamburger");
+const navigation = document.querySelector(".navigation");
 
-const dropdownButton = document.querySelector('.dropdown-button');
-const dropdownMenu = document.querySelector('.dropdown__list');
+const dropdownButton = document.querySelector(".dropdown-button");
+const dropdownMenu = document.querySelector(".dropdown__list");
 
-hamburger.addEventListener('click', (e) => {
-   navigation.classList.toggle('visible');
+hamburger.addEventListener("click", (e) => {
+  navigation.classList.toggle("visible");
 });
 
-dropdownButton.addEventListener('click', (e) => {
-   dropdownMenu.classList.toggle('visible');
+dropdownButton.addEventListener("click", (e) => {
+  dropdownMenu.classList.toggle("visible");
 });
