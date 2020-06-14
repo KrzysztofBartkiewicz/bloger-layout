a0ffd03a284033b978b40fc608e6524a0a3225c6
Krzysztof Bartkiewicz <bartkiewicz.krzysztof84@gmail.com>
2020-06-14 14:46:54 +0200

    added logic for dropdown menu hiding effect

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ const navigation = document.querySelector(".navigation");
 
 const dropdownButton = document.querySelector(".dropdown-button");
 const dropdownMenu = document.querySelector(".dropdown__list");
-console.dir("dropdownMenu", dropdownMenu)
+
+let buttonClicked = false;
 
 hamburger.addEventListener("click", (e) => {
   navigation.classList.toggle("visible");
@@ -11,4 +12,20 @@ hamburger.addEventListener("click", (e) => {
 
 dropdownButton.addEventListener("click", (e) => {
   dropdownMenu.classList.toggle("dropdown-visible");
+  buttonClicked = true;
+});
+
+//console.log(dropdownMenu.getBoundingClientRect());
+
+document.addEventListener('click', (e) => {
+   if (dropdownMenu.classList.contains('dropdown-visible') && !buttonClicked) {
+      const coords = dropdownMenu.getBoundingClientRect();
+      if (!(e.clientX > coords.left &&
+            e.clientX < coords.right &&
+            e.clientY > coords.top &&
+            e.clientY < coords.bottom)) {
+         dropdownMenu.classList.remove('dropdown-visible');
+      }
+   }
+   buttonClicked = false;
 });
