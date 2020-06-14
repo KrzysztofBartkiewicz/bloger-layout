531e4cab5b199ca3d749655556fdc64c5cd7f4b4
Krzysztof Bartkiewicz <bartkiewicz.krzysztof84@gmail.com>
2020-06-14 14:04:27 +0200

    added animation for navigation

--- js/main.js
+++ js/main.js
@@ -3,11 +3,12 @@ const navigation = document.querySelector(".navigation");
 
 const dropdownButton = document.querySelector(".dropdown-button");
 const dropdownMenu = document.querySelector(".dropdown__list");
+console.dir("dropdownMenu", dropdownMenu)
 
 hamburger.addEventListener("click", (e) => {
   navigation.classList.toggle("visible");
 });
 
 dropdownButton.addEventListener("click", (e) => {
-  dropdownMenu.classList.toggle("visible");
+  dropdownMenu.classList.toggle("dropdown-visible");
 });
