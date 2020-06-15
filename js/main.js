b804d83abfd64ea6b3fb98062dcfc438a136017f
Krzysztof Bartkiewicz <bartkiewicz.krzysztof84@gmail.com>
2020-06-15 16:17:45 +0200

    fix dropdown animation

--- js/main.js
+++ js/main.js
@@ -7,27 +7,34 @@ const dropdownMenu = document.querySelector(".dropdown__list");
 let isButtonClicked = false;
 
 hamburger.addEventListener("click", (e) => {
-  navigation.classList.toggle("visible");
-  hamburger.classList.toggle('hamburger--active');
+   navigation.classList.toggle("visible");
+   hamburger.classList.toggle("hamburger--active");
 });
 
 dropdownButton.addEventListener("click", (e) => {
-  dropdownMenu.classList.toggle("dropdown-visible");
-  isButtonClicked = true;
+   dropdownMenu.classList.toggle('display-block');
+   dropdownMenu.classList.toggle("dropdown-visible");
+   isButtonClicked = true;
 });
 
 //console.log(dropdownMenu.getBoundingClientRect());
 
-document.addEventListener('click', (e) => {
-   if (dropdownMenu.classList.contains('dropdown-visible') && !isButtonClicked) {
+document.addEventListener("click", (e) => {
+   if (
+      dropdownMenu.classList.contains("dropdown-visible") &&
+      !isButtonClicked
+   ) {
       const coords = dropdownMenu.getBoundingClientRect();
-      if (!(e.clientX > coords.left &&
+      if (
+         !(
+            e.clientX > coords.left &&
             e.clientX < coords.right &&
             e.clientY > coords.top &&
-            e.clientY < coords.bottom)) {
-         dropdownMenu.classList.remove('dropdown-visible');
+            e.clientY < coords.bottom
+         )
+      ) {
+         dropdownMenu.classList.remove("dropdown-visible");
       }
    }
    isButtonClicked = false;
 });
-
